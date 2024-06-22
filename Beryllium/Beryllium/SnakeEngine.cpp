127cee9da7e441d1886954fad2256134f4802389
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-06-21 21:49:55 -0400

    remove bracket for the snake engine class

--- Beryllium/Beryllium/SnakeEngine.cpp
+++ Beryllium/Beryllium/SnakeEngine.cpp
@@ -1,4 +1,4 @@
-#include "SnakeEngine.h"<
+#include "SnakeEngine.h"
 
 SnakeEngine::SnakeEngine(size_t imageWidth, size_t imageHeight, uint32_t* imageData)
     :m_imageWidth(imageWidth), m_imageHeight(imageHeight), m_imageData(imageData){}
